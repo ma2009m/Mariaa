2203a739f66fe64c78e913a6c849d8c362e3a9b7
Isabella <pratesisabela471@gmail.com>
2024-07-02 16:56:40 -0300

    beecrowd

---
+++ MEDIA2.py
@@ -0,0 +1,7 @@
+a = float(input("portugues"))
+b = float(input("matematica"))
+c = float(input("historia"))
+
+media = (a+b+c) /3
+
+print("media =",media)
---
+++ salario.py
@@ -0,0 +1,12 @@
+funcionario = int(input('numero do funcionario'))
+horas = float(input('horas trabalhadas'))
+valor = float(input('valor de cada hora')) 
+
+
+
+print("NUMBER =", funcionario)
+
+
+salario = valor*horas
+print(NUMBER)
+
